9f05b8dc8a5b81893ee32fe19fb31c184e715f32
Gescof <gescolarfernandez@gmail.com>
2023-02-08 16:35:52 +0100

    Add logs for CustomersController

--- backend/src/main/java/com/gescof/springbootpostgrecompose/controllers/CustomersController.java
+++ backend/src/main/java/com/gescof/springbootpostgrecompose/controllers/CustomersController.java
@@ -4,6 +4,7 @@ import com.gescof.springbootpostgrecompose.models.CustomerRequest;
 import com.gescof.springbootpostgrecompose.models.CustomerResponse;
 import com.gescof.springbootpostgrecompose.services.CustomersService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Log4j2
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("api/v1/customers/")
@@ -24,25 +26,26 @@ public class CustomersController {
 
     @GetMapping
     public ResponseEntity<List<CustomerResponse>> getCustomers() {
+        log.info("GET /customers");
         return ResponseEntity.ok(customersService.getCustomers());
     }
 
     @PostMapping
-    public ResponseEntity<Long> createCustomer(
-            @RequestBody CustomerRequest customerRequest) {
+    public ResponseEntity<Long> createCustomer(@RequestBody CustomerRequest customerRequest) {
+        log.info("POST /customers");
         return ResponseEntity.ok(customersService.createCustomer(customerRequest));
     }
 
     @PutMapping("{customerId}")
-    public ResponseEntity<Long> updateCustomer(
-            @PathVariable Long customerId,
-            @RequestBody CustomerRequest customerRequest) {
+    public ResponseEntity<Long> updateCustomer(@PathVariable Long customerId,
+                                               @RequestBody CustomerRequest customerRequest) {
+        log.info("PUT /customers/{}", customerId);
         return ResponseEntity.ok(customersService.updateCustomer(customerId, customerRequest));
     }
 
     @DeleteMapping("{customerId}")
-    public ResponseEntity<Boolean> deleteCustomer(
-            @PathVariable Long customerId) {
+    public ResponseEntity<Boolean> deleteCustomer(@PathVariable Long customerId) {
+        log.info("DELETE /customers/{}", customerId);
         return ResponseEntity.ok(customersService.deleteCustomer(customerId));
     }
 }
